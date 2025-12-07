e08f6fd3a86f99b76d8013487beb9eb3b0a440c4
michaelfernande <michaelfernande@hotmail.com>
2025-12-07 13:36:11 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -17,7 +17,6 @@ export class InternalServerError extends Error {
     };
   }
 }
-
 export class ServiceError extends Error {
   constructor({ cause, message }) {
     super(message || "Serviço indisponivel no momento.", {
@@ -26,6 +25,25 @@ export class ServiceError extends Error {
     this.name = "ServiceError";
     this.action = "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados e tente novamente.";
+    this.statusCode = 400;
   }
 
   toJSON() {
